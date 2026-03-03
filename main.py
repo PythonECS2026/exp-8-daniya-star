9cc350e69ad6f539f14089dfb847321bfd2c6ac5
daniya-star <daniya@eng.rizvi.edu.in>
2026-03-03 21:35:11 +0530

    Refactor triangle pattern program with error handling
    Added input prompt and error handling for user input.

--- main.py
+++ main.py
@@ -4,15 +4,18 @@
 # Coder:khan daniya rehman
 # Date:03/03/2026
 
-print("--- Pattern Printer ---")
+print("--- Pattern Printer ---\n")
 
 
 # Write your code here 
-n = int(input())
-for i in range(1, n + 1):
-    for j in range(i):
-        print("*", end="")
-    print()
+try:
+    n = int(input("Enter number of lines: "))
+
+    for i in range(1, n + 1):
+        print("*" * i)
+
+except ValueError:
+    print("Invalid input! Please enter integer only.")
 
 
 
