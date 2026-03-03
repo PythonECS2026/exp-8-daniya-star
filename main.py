a1d085cec3afb2b9a411325bfcd3dac0f4c1b72b
daniya-star <daniya@eng.rizvi.edu.in>
2026-03-03 19:57:05 +0530

    Refactor triangle pattern printing logic

--- main.py
+++ main.py
@@ -8,20 +8,12 @@ print("--- Pattern Printer ---\n")
 
 
 # Write your code here
-'''
-n=int(input("enter a number:-"))
-for i in range(1,n+1):
-   for j in range(1,1+1):
-     print("*", end="")
-   print(" ")
-'''
- print("--- Pattern Printer ---\n")
+n = int(input())
+for i in range(1, n + 1):
+    for j in range(i):
+        print("*", end="")
+    print()
 
-#Write your code here
-
-n=int(input("Enter Rows:"))
-for i in range(1,n+1):
-    print("*"*1)
 
 
 
