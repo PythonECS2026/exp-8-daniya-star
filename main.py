c340b90c7bda42c62489800323c54c689de994ac
daniya-star <daniya@eng.rizvi.edu.in>
2026-03-03 12:58:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -8,8 +8,7 @@ print("--- Pattern Printer ---\n")
 
 
 # Write your code here
-n = int(input())
-for i in range(1, n + 1):
-    for j in range(i):
-        print("*", end="")
-    print()
+n=int(input("enter rows:"))
+for i in range(1,n+1):
+    print("*"*i)
+
